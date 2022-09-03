9439fe26f13c3c6e922ed2abe4cadd3b54abbc42
Anuradha <anuradhasengupta9874@gmail.com>
2022-09-03 22:53:17 +0530

    Dark Mode Enabled

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import About from "./About";
+import Alert from "./Alert";
 import "./App.css";
 import Navbar from "./Navbar.jsx";
 import TextBox from "./TextBox.jsx";
 
 
-function App() {
 
+function App()
+{
+    const [presentmode,changeMode]=useState('light')
+    const [presentAlert,set_Alert]=useState(null)
+
+    const toggleMode=()=>
+    {
+        if(presentmode==='light')
+        {
+            
+            changeMode('dark');
+        }
+        else
+        {
+            
+            changeMode('light');
+            
+        }
+    }
+    console.log(presentmode+" ");
     return (
         <div>
-            {/*----<Navbar title='TextUtils' abouthead="About Us" />
-            //<TextBox boxhead="Enter your text here"/>--*/}
-            <About/>
+            <Navbar title='TextUtils' mode={presentmode} abouthead="About Us" togglemode={toggleMode}/>
+            <TextBox boxhead="Enter your text here"/>
+            {/*<About/>*/}
         </div>
     );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types';
-export default function Navbar(props) {
+export default function Navbar(props) 
+{
+    const [myStyle,set_Color] = useState({
+        color:"white",
+        backgroundColor:"black"
+    })
+    const [currentMode,newMode]=useState(props.mode)
+
+
+    const changeMode=()=>
+    {
+        if(currentMode==='light')
+        {
+            set_Color({color:'black',backgroundColor:'grey'})
+            document.body.style.backgroundColor='black';
+            newMode('dark')
+            
+        }
+        else
+        {
+            set_Color({
+                color:"white",
+                backgroundColor:"black",
+            });
+            document.body.style.backgroundColor='white'
+            newMode('light');
+        }
+    }
+    
+
     return (
-        <nav className="navbar navbar-expand-lg bg-dark" style={{color:"white"}}>
-            <a className="navbar-brand" href="/">{props.title}</a>
+        <nav className="navbar navbar-expand-lg" style={myStyle}>
+            <a className="navbar-brand" href="/" style={myStyle}>{props.title}</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
 
+
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
-                        <a className="nav-link" href="/">Home </a>
+                        <a className="nav-link" href="/" style={myStyle}>Home </a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/">{props.abouthead}</a>
+                        <a className="nav-link" href="/" style={myStyle}>About</a>
                     </li>
+
                 </ul>
-            </div>
+                </div>
+                
+                <div className="form-check form-switch">
+                    <input type="checkbox" className="form-check-input" id="flexSwitchCheckDefault"  onClick={changeMode}/>
+                    <label className="form-check-label" htmlFor="flexSwitchCheckDefault" >Enable Dark Mode</label>
+                </div>
+            
+
         </nav>
     )
 }
